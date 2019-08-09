4c7a3fec35f7cfb0f74115180913f69b77082110
theVizi0n <jkrenninger@gmail.com>
2019-08-08 22:50:43 -0400

    Add files via upload

---
+++ tic_tac_toe.py
@@ -0,0 +1,62 @@
+'''
+Tic Tac Toe
+'''
+
+
+def start_game():
+	board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ',' ', ' ', ' ', ' ']
+
+	def display_board():
+		print(board[7] + '|' + board[8] + '|' + board[9])
+		print(board[4] + '|' + board[5] + '|' + board[6])
+		print(board[1] + '|' + board[2] + '|' + board[3])
+
+	marker1= ''
+	marker2= ''
+	player1 = input("Please pick marker: 'X' or 'O'")
+	if 'x':
+		marker1 = 'X'
+		marker2 = '0'
+		print("'Player 1 has chosen 'X', Player 2 will be 'O'")
+	elif 'o':
+		marker1 = 'O'
+		marker2 = 'X'
+		print("'Player 1 has chosen 'O', Player 2 will be 'X'")
+	
+	display_board()
+	
+	player1_turn():
+		pass
+	player2_turn():
+		pass
+		
+	def choose_space():
+		space = input('Using your num pad, where would you like to place your marker?')
+		if space == '1':
+			board[1] = marker1
+		elif space == '2':
+			board[2] = marker1
+		elif space == '3':
+			board[3] = marker1
+		elif space == '4':
+			board[4] = marker1
+		elif space == '5':
+			board[5] = marker1
+		elif space == '6':
+			board[6] = marker1
+		elif space == '7':
+			board[7] = marker1
+		elif space == '8':
+			board[8] = marker1
+		elif space == '9':
+			board[9] = marker1
+		else:
+			pass
+		display_board()
+	
+	choose_space()
+
+	
+
+
+start_game()
